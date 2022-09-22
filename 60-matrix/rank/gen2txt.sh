41aadaf7576b2dba23d2eaa71e8dd393e35007ca
Lucas Peres <lucasperes95@gmail.com>
2022-09-22 10:22:02 -0300

    fixing gen2txt: each line has an adjacency instead of a gene

--- 60-matrix/rank/gen2txt.sh
+++ 60-matrix/rank/gen2txt.sh
@@ -1,3 +1,15 @@
 #!/usr/bin/bash
 
-cat $1 | sed 's/^.\(.*\).$/\1/' | awk '{for (i=1; i<= NF; i=i+2) print $i " " $(i+1)}'
+# If the first char is '(', then the chromosome is circular;
+# in this case, the 1st extremity is adjacent to the last extremity.
+# Print the extremities in between.
+# Notice that we start at 3 to skip the enclosing braces or brackets, and
+# the 1st extremity.
+# In the case of linear genomes, the telomeres are ignored.
+cat $1 |
+    awk '{
+        if ($1=="(") 
+            print $2 " " $(NF-1);
+        for (i=3; i<NF-1; i=i+2)
+            print $i " " $(i+1)
+    }'
