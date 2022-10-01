e7bba3f2702e3d810eda3f6caefedbb83ca7854d
Lucas Peres <lucasperes95@gmail.com>
2022-10-01 20:14:35 -0300

    refactoring: each conf appears as a string with lower and upper case letters for parameters, e.g. gXieZ

--- simulation/experiment.py
+++ simulation/experiment.py
@@ -10,21 +10,30 @@ if len(sys.argv) != 3:
     raise SystemExit('usage: ./experiment.py [parameters file] [number of iterations]')
 
 with open(sys.argv[1], 'r') as fin:
-    params = dict()
+    params = []
     for line in fin:
         line = line.strip()
         if line and not line.startswith('#'):
-            k, *v = line.split()
-            params[k] = v
+            # 1st: parameter name; 2nd: low value; 3rd: high value
+            # e.g. 'g 5000 10000'
+            k, low, high = line.split()
+            param = dict()
+            param[k.lower()] = low
+            param[k.upper()] = high
+            params.append(param)
 
 niters = int(sys.argv[2])
 
-for k, conf in enumerate(itertools.product(*params.values())):
-    # be careful with the ordering
-    g, x, i, e, zipf = conf
-    for j in tqdm.tqdm(range(niters), ascii=' #'):
+for keys in tqdm.tqdm(itertools.product(*params), ascii=' #'):
+    conf = ''.join(keys)
+
+    # WARNING: an ordering is expected in params, and, therefore, 
+    # in the conf file!
+    g, x, i, e, z = [d[k] for k, d in zip(keys, params)]
+
+    for j in range(niters):
         try:
-            subprocess.check_output(f'make rank_tree NGENES={g} NCHRS={x} INSERTION_RATE={i} DELETION_RATE={e} INDEL_SIZE={zipf} -B', shell=True)
-            shutil.move('rank/rank_tree.nwk', f'rank/rank_tree_conf{k}_iter{j}.nwk')
+            subprocess.check_output(f'make rank_tree NGENES={g} NCHRS={x} INSERTION_RATE={i} DELETION_RATE={e} INDEL_SIZE={z} -B', shell=True)
+            shutil.move('rank/rank_tree.nwk', f'rank/rank_tree_{conf}_iter{j+1}.nwk')
         except subprocess.CalledProcessError as e:
             print(e.output, e.returncode)
