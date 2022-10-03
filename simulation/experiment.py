a93d17d02fe65d65621f67f3a052affb048049e6
Lucas Peres <lucasperes95@gmail.com>
2022-10-03 11:46:15 -0300

    adding output messages

--- simulation/experiment.py
+++ simulation/experiment.py
@@ -4,7 +4,6 @@ import itertools
 import sys
 import subprocess
 import shutil
-import tqdm
 
 if len(sys.argv) != 3:
     raise SystemExit('usage: ./experiment.py [parameters file] [number of iterations]')
@@ -24,16 +23,20 @@ with open(sys.argv[1], 'r') as fin:
 
 niters = int(sys.argv[2])
 
-for keys in tqdm.tqdm(itertools.product(*params), ascii=' #'):
+for keys in itertools.product(*params):
     conf = ''.join(keys)
+    print(f'Running configuration {conf}')
 
     # WARNING: an ordering is expected in params, and, therefore, 
     # in the conf file!
     g, x, i, e, z = [d[k] for k, d in zip(keys, params)]
 
     for j in range(niters):
+        print(f'Iteration {j+1}')
         try:
-            subprocess.check_output(f'make rank_tree NGENES={g} NCHRS={x} INSERTION_RATE={i} DELETION_RATE={e} INDEL_SIZE={z} -B', shell=True)
+            cmd = f'make rank_tree NGENES={g} NCHRS={x} INSERTION_RATE={i} DELETION_RATE={e} INDEL_SIZE={z} -B'
+            print(cmd)
+            subprocess.check_output(cmd, shell=True)
             shutil.move('rank/rank_tree.nwk', f'rank/rank_tree_{conf}_iter{j+1}.nwk')
         except subprocess.CalledProcessError as e:
             print(e.output, e.returncode)
