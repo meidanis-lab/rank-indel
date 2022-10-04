9222bd2752cb62a996602be39fdde0320220115a
Lucas Peres <lucasperes95@gmail.com>
2022-10-04 11:19:34 -0300

    adding flag for distance model; updating all strings that use dist

--- simulation/experiment.py
+++ simulation/experiment.py
@@ -5,10 +5,13 @@ import sys
 import subprocess
 import shutil
 
-if len(sys.argv) != 3:
-    raise SystemExit('usage: ./experiment.py [parameters file] [number of iterations]')
+if len(sys.argv) != 4:
+    raise SystemExit('usage: ./experiment.py [distance model: rank or dcj] [parameters file] [number of iterations]')
 
-with open(sys.argv[1], 'r') as fin:
+if sys.argv[1] not in {'rank', 'dcj'}:
+    raise SystemExit('Invalid distance model: must be either \'dcj\' or \'rank\'')
+
+with open(sys.argv[2], 'r') as fin:
     params = []
     for line in fin:
         line = line.strip()
@@ -21,7 +24,8 @@ with open(sys.argv[1], 'r') as fin:
             param[k.upper()] = high
             params.append(param)
 
-niters = int(sys.argv[2])
+niters = int(sys.argv[3])
+dist = sys.argv[1]
 
 for keys in itertools.product(*params):
     conf = ''.join(keys)
@@ -34,9 +38,9 @@ for keys in itertools.product(*params):
     for j in range(niters):
         print(f'Iteration {j+1}')
         try:
-            cmd = f'make rank_tree NGENES={g} NCHRS={x} INSERTION_RATE={i} DELETION_RATE={e} INDEL_SIZE={z} -B'
+            cmd = f'make {dist}_tree NGENES={g} NCHRS={x} INSERTION_RATE={i} DELETION_RATE={e} INDEL_SIZE={z} -B'
             print(cmd)
             subprocess.check_output(cmd, shell=True)
-            shutil.move('rank/rank_tree.nwk', f'rank/rank_tree_{conf}_iter{j+1}.nwk')
+            shutil.move(f'{dist}/{dist}_tree.nwk', f'{dist}/{dist}_tree_{conf}_iter{j+1}.nwk')
         except subprocess.CalledProcessError as e:
             print(e.output, e.returncode)
