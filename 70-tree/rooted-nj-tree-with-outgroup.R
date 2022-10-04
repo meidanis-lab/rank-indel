23629e9bd8efd9c96503287497b8be88751cbc8e
Lucas Peres <lucasperes95@gmail.com>
2022-10-04 09:54:47 -0300

    removing commented sections

--- 70-tree/rooted-nj-tree-with-outgroup.R
+++ 70-tree/rooted-nj-tree-with-outgroup.R
@@ -1,7 +1,6 @@
 #!/usr/bin/env Rscript
 
 library(ape)
-#library(ggtree)
 
 # args[1] expects the distance matrix in CSV format as provided by 60-matrix
 # args[2] expects the name for the output file (no extension)
@@ -16,11 +15,3 @@ tree = ape::nj(as.matrix(mat))
 tree = ape::root(tree, args[3], resolve.root=TRUE)
 
 write.tree(tree, file=paste(args[2], ".nwk", sep=""))
-
-# This step will be done in an interactive session (e.g. jupyter notebook)
-# png(filename=paste(args[2], ".png", sep=""))
-# plot = ggtree(tree, branch.length="none", layout="circular") + 
-#         geom_tiplab() + 
-#         theme(plot.margin=unit(c(50,44,50,44),"mm"))
-# print(plot)
-# dev.off()
