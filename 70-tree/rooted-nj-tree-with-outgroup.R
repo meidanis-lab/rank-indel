0c361dffc58f2a181a9e9866b7073c168220056f
Lucas Peres <lucasperes95@gmail.com>
2022-10-04 10:09:18 -0300

    changing assignment symbol to <-

--- 70-tree/rooted-nj-tree-with-outgroup.R
+++ 70-tree/rooted-nj-tree-with-outgroup.R
@@ -5,13 +5,13 @@ library(ape)
 # args[1] expects the distance matrix in CSV format as provided by 60-matrix
 # args[2] expects the name for the output file (no extension)
 # args[3] expects the label of the outgroup
-args = commandArgs(trailingOnly=TRUE)
+args <- commandArgs(trailingOnly=TRUE)
 
-mat = read.csv(args[1], header=TRUE, row.names=1)
-mat[is.na(mat)] = 0
-mat = as.dist(mat)
+mat <- read.csv(args[1], header=TRUE, row.names=1)
+mat[is.na(mat)] <- 0
+mat <- as.dist(mat)
 
-tree = ape::nj(as.matrix(mat))
-tree = ape::root(tree, args[3], resolve.root=TRUE)
+tree <- ape::nj(as.matrix(mat))
+tree <- ape::root(tree, args[3], resolve.root=TRUE)
 
 write.tree(tree, file=paste(args[2], ".nwk", sep=""))
