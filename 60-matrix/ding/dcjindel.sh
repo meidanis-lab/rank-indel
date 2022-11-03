941e5e94467181752aec790ac4b789287580ceea
Lucas Peres <lucasperes95@gmail.com>
2022-11-03 01:52:16 -0300

    added PARENT_PATH before every path variable

--- 60-matrix/ding/dcjindel.sh
+++ 60-matrix/ding/dcjindel.sh
@@ -11,13 +11,13 @@ PARENT_PATH=$(cd $(dirname "${BASH_SOURCE[0]}"); pwd -P)
 
 ${PARENT_PATH}/genome2unimog.py --input $1 $2 --output ${PARENT_PATH}/${UNIMOG_FILE_NAME}.unimog
 
-${PARENT_PATH}/unimog_to_ilp.py -i ${UNIMOG_FILE_NAME}.unimog -u ${UNIMOG_FILE_NAME}.ids -o ${UNIMOG_FILE_NAME}.lp 2> /dev/null
+${PARENT_PATH}/unimog_to_ilp.py -i ${PARENT_PATH}/${UNIMOG_FILE_NAME}.unimog -u ${PARENT_PATH}/${UNIMOG_FILE_NAME}.ids -o ${PARENT_PATH}/${UNIMOG_FILE_NAME}.lp 2> /dev/null
 
-gurobi_cl ResultFile=${UNIMOG_FILE_NAME}.sol ${UNIMOG_FILE_NAME}.lp > /dev/null
+gurobi_cl ResultFile=${PARENT_PATH}/${UNIMOG_FILE_NAME}.sol ${PARENT_PATH}/${UNIMOG_FILE_NAME}.lp > /dev/null
 
 DISTANCE=$(${PARENT_PATH}/parse_gurobi_sol.py \
-    -i ${UNIMOG_FILE_NAME}.sol \
-    -u ${UNIMOG_FILE_NAME}.ids \
-    -o ${UNIMOG_FILE_NAME}_relabeled.unimog | cut -f 3 | tail -n 1)
+    -i ${PARENT_PATH}/${UNIMOG_FILE_NAME}.sol \
+    -u ${PARENT_PATH}/${UNIMOG_FILE_NAME}.ids \
+    -o ${PARENT_PATH}/${UNIMOG_FILE_NAME}_relabeled.unimog | cut -f 3 | tail -n 1)
 
 echo -e "${GENOME_A}\t${GENOME_B}\t${DISTANCE}"
