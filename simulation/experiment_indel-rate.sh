7e4763f5e5ddeb8adb3adba2ae57363052d2d6d6
Lucas Peres <lucasperes95@gmail.com>
2022-11-03 00:07:09 -0300

    added script to run experiment that varies indel rate and fixes all other parameters; in addition, each setting runs 10 times

---
+++ simulation/experiment_indel-rate.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/bash
+
+if [[ $1 == "dcj" ]]
+then
+    DIST="dcj"
+elif [[ $(expr substr $1 1 4) == "rank" ]]
+then
+    DIST="rank"
+fi
+
+for i in $(seq 0.0 0.1 0.9)
+do
+    for j in $(seq 10)
+    do
+        make -B $1 INSRATE=${i} DELRATE=${i};
+        mv ${DIST}/$1_tree.nwk ${DIST}/$1_tree_indel-${i}_iter${j}.nwk;
+        mv ${DIST}/$1_tree_indel-${i}_iter${j}.nwk ${DIST}/results_for_indel-rate
+    done
+done
