92b0704a41be0fc7accf7ba30752a33560a6fc89
Lucas Peres <lucasperes95@gmail.com>
2022-11-04 02:13:13 -0300

    adding functions to format genome identifier according to PHYLIP format: must have exactly 10 characters

--- 60-matrix/unimog/gen2unimog.py
+++ 60-matrix/unimog/gen2unimog.py
@@ -19,5 +19,11 @@ with fileinput.input() as fin:
             chrom.append('|')
         genome.append(' '.join(chrom))
 
-fmt_genome_name = lambda path: path.split('/')[-1].split('.')[0]
-print('>' + fmt_genome_name(fin.filename()) + '\n' + '\n'.join(genome))
+## for PHYLIP, identifier must have exactly 10 characters
+## we adopt the following convention: E_coli_123456789 ==> Ec12345678
+## that is, one upper letter for genus, one upper letter for species, and 8 letters for PREFIX of strain
+suffix = lambda ident: ident.split('_')[-1][:6]
+prefix = lambda ident: ident[0] + ident[2].upper()
+get_organism = lambda path: path.split('/')[-1].split('.')[0]
+fmt_ident = lambda organism: prefix(organism) + '_' + suffix(organism)
+print('>' + fmt_ident(get_organism(fin.filename())) + '\n' + '\n'.join(genome))
