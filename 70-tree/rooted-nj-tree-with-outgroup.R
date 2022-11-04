64fd50cf28f58569de79c29d93165e19071fd133
Lucas Peres <lucasperes95@gmail.com>
2022-11-04 02:40:39 -0300

    adding conditional and 4th argument: user passes matrix format and script treats accordingly

--- 70-tree/rooted-nj-tree-with-outgroup.R
+++ 70-tree/rooted-nj-tree-with-outgroup.R
@@ -1,17 +1,25 @@
 #!/usr/bin/env Rscript
 
 library(ape)
+library(phangorn)
 
-# args[1] expects the distance matrix in CSV format as provided by 60-matrix
+# args[1] expects the distance matrix format specified by args[4]
 # args[2] expects the name for the output file (no extension)
 # args[3] expects the label of the outgroup
+# args[4] 'csv' or 'phylip' (as provided by 'triangular2square.awk')
 args <- commandArgs(trailingOnly=TRUE)
 
-mat <- read.csv(args[1], header=TRUE, row.names=1)
-mat[is.na(mat)] <- 0
-mat <- as.dist(mat)
+if (args[4] == "csv") {
+    mat <- read.csv(args[1], header=TRUE, row.names=1)
+    mat[is.na(mat)] <- 0
+    mat <- as.matrix(as.dist(mat))
+} else if (args[4] == "phylip") {
+    mat <- phangorn::readDist(args[1], format="phylip")
+} else {
+    stop("matrix format must be either 'csv' or 'phylip'")
+}
 
-tree <- ape::nj(as.matrix(mat))
+tree <- ape::nj(mat)
 tree <- ape::root(tree, args[3], resolve.root=TRUE)
 
 write.tree(tree, file=paste(args[2], ".nwk", sep=""))
